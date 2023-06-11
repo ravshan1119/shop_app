bd92c9103db6842d63ac1bc2e3fde84d4dee775f
Homework <allamurodovravshan38@gmail.com>
2023-06-12 02:37:40 +0500

    Initial commit

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,53 @@
+class AppImages {
+  // img
+  static String bank = "assets/images/bank.png";
+  static String block = "assets/images/block.png";
+  static String mastercard = "assets/images/mastercard.png";
+  static String paypal = "assets/images/paypal.png";
+  static String undrawOrderConfirmed =
+      "assets/images/undraw_order_confirmed.png";
+
+
+  // svg
+  static String adidas = "assets/svg/adidas.svg";
+  static String arrowDown = "assets/svg/arrow_down.svg";
+  static String arrowRight = "assets/svg/arrow_right.svg";
+  static String arrowUp = "assets/svg/arrow_up.svg";
+  static String back = "assets/svg/back.svg";
+  static String badge = "assets/svg/badge.svg";
+  static String bag = "assets/svg/bag.svg";
+  static String bag2 = "assets/svg/bag2.svg";
+  static String bag_2 = "assets/svg/bag_2.svg";
+  static String chack = "assets/svg/chack.svg";
+  static String check = "assets/svg/check.svg";
+  static String clock = "assets/svg/clock.svg";
+  static String delete = "assets/svg/delete.svg";
+  static String edit = "assets/svg/edit.svg";
+  static String edit_2 = "assets/svg/edit_2.svg";
+  static String ellipse = "assets/svg/ellipse.svg";
+  static String facebook = "assets/svg/facebook.svg";
+  static String fila = "assets/svg/fila.svg";
+  static String google = "assets/svg/google.svg";
+  static String heart = "assets/svg/heart.svg";
+  static String heart_2 = "assets/svg/heart_2.svg";
+  static String info = "assets/svg/info.svg";
+  static String lock = "assets/svg/lock.svg";
+  static String logo = "assets/svg/logo.svg";
+  static String logout = "assets/svg/logout.svg";
+  static String menu = "assets/svg/menu.svg";
+  static String menu_2 = "assets/svg/menu_2.svg";
+  static String nike = "assets/svg/nike.svg";
+  static String search = "assets/svg/search.svg";
+  static String settings = "assets/svg/settings.svg";
+  static String sort = "assets/svg/sort.svg";
+  static String star = "assets/svg/star.svg";
+  static String star2 = "assets/svg/star2.svg";
+  static String sun = "assets/svg/sun.svg";
+  static String switchBase = "assets/svg/switch_base.svg";
+  static String switchIndecator = "assets/svg/switch_indecator.svg";
+  static String twit = "assets/svg/twit.svg";
+  static String visa = "assets/svg/visa.svg";
+  static String voice = "assets/svg/voice.svg";
+  static String wallet = "assets/svg/wallet.svg";
+  static String wallet_2 = "assets/svg/wallet_2.svg";
+}
