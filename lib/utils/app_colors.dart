3fc8885a8b2364f8a17ec182c057b104f8b11136
Homework <allamurodovravshan38@gmail.com>
2023-06-12 02:49:43 +0500

    part 1

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors{
+  static Color C_9775FA = const Color(0xFF9775FA);
+  static Color C_4267B2 = const Color(0xFF4267B2);
+  static Color C_1DA1F2 = const Color(0xFF1DA1F2);
+  static Color C_EA4335 = const Color(0xFFEA4335);
+  static Color C_8F959E = const Color(0xFF8F959E);
+  static Color C_1D1E20 = const Color(0xFF1D1E20);
+  static Color C_34C559 = const Color(0xFF34C559);
+  static Color C_E7E8EA = const Color(0xFFE7E8EA);
+  static Color C_FF981F = const Color(0xFFFF981F);
+  static Color C_F5F6FA = const Color(0xFFF5F6FA);
+}
